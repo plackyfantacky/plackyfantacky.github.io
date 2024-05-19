342e716b99cd21d2a31910fb921cc2eb2c9e3862
Adam Trickett <adamt@workhouse.au>
2024-05-19 17:24:57 +0800

    initial commit. hello world

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    
+
+    return (
+        <>
+            <header>
+                <h1>Scratchpad by @plackyfantacky</h1>
+            </header>
+            <main>
+                <p>stuff will go here eventually</p>
+            </main>
+            <footer>
+
+            </footer>
+        </>              
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            'sans': ['"Source Sans 3"', 'sans-serif'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
