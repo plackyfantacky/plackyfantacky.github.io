56df2eb7e66d33f9318b8c7e5b829c41ef658a2b
Adam Trickett <adamt@workhouse.au>
2024-05-26 08:28:48 +0800

    update ci script 4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://plackyfantacky.github.io/',
-  plugins: [react()],
+    plugins: [react()],
+    base: 'https://plackyfantacky.github.io/dist/',
 })
